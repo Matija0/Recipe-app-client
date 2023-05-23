40f75898fa60eba85b8de4faf603df7c60e08b11
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-05-23 22:09:05 +0200

    save recipe, auth verify token

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {useCookies} from "react-cookie"
+import { useCookies } from "react-cookie";
 
 const Navbar = () => {
-  const [cookies, setCookies]=useCookies(["access_token"])
-  const navigate= useNavigate()
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
 
-  const logout= () =>{
-    setCookies("access_token", "")
-    window.localStorage.removeItem("userID")
-    navigate("/login")
-    
-  }
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigate("/login");
+  };
   return (
     <div className="navbar">
       <Link to={"/"}>Home</Link>
       <Link to={"/createrecipe"}>Create Recipe</Link>
-      <Link to={"/savedrecipes"}>Saved Recipes</Link>
-      {!cookies.access_token? (<div><Link to={"/login"}>Login</Link>
-      <Link to={"/register"}>Register</Link></div>) : (<button onClick={logout}>Logout</button>)}
-      
+
+      {!cookies.access_token ? (
+        <div>
+          <Link to={"/login"}>Login</Link>
+          <Link to={"/register"}>Register</Link>
+        </div>
+      ) : (
+        <>
+        <Link to={"/savedrecipes"}>Saved Recipes</Link>
+        <button onClick={logout}>Logout</button>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/CreateRecipe.js
+++ src/pages/CreateRecipe.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import { useGetUserID } from '../hooks/useGetUserID';
-import { useNavigate } from 'react-router';
+import axios from "axios";
+import React, { useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import { useNavigate } from "react-router";
+import {useCookies} from "react-cookie"
 
 const CreateRecipe = () => {
-  const userID= useGetUserID()
-
+  const userID = useGetUserID();
+  const [cookies,setCookies]=useCookies(["access_token"])
   const [recipe, setRecipe] = useState({
     name: "",
     description: "",
@@ -16,58 +17,79 @@ const CreateRecipe = () => {
     userOwner: userID,
   });
 
-  const navigate=useNavigate()  
+  const navigate = useNavigate();
 
-  const handleChange = (event) =>{
-    const {name, value}=event.target
-    setRecipe({...recipe,[name]:value})
-  }
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
 
-  const handleIngredientChange = (e,index) =>{
-    const {value}=e.target
-    const ingredients=recipe.ingredients
-    ingredients[index]=value
-    setRecipe({...recipe,ingredients})
-  
-  }
+  const handleIngredientChange = (e, index) => {
+    const { value } = e.target;
+    const ingredients = recipe.ingredients;
+    ingredients[index] = value;
+    setRecipe({ ...recipe, ingredients });
+  };
 
-  const addIngredient= () =>{
-    setRecipe({...recipe, ingredients: [...recipe.ingredients, ""]})
-  }
-  
-  const handleSubmit = async(event) =>{
-    event.preventDefault()
-    try{
-      await axios.post("http://localhost:3001/recipes", recipe)
-      alert("Recipe Created")
-      navigate("/")
-    }catch(err){
-      console.error(err)
+  const addIngredient = () => {
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/recipes", recipe, {
+        headers: { authorization: cookies.access_token },
+      });
+      alert("Recipe Created");
+      navigate("/");
+    } catch (err) {
+      console.error(err);
     }
-  }
+  };
   return (
-    <div className='create-recipe'>
+    <div className="create-recipe">
       <h2>Create Recipe</h2>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input id='name' name='name' onChange={handleChange}/>
-        <label htmlFor='description'>Description</label>
-        <textarea id='description' name='description' onChange={handleChange}></textarea>
-        <label htmlFor='ingredients'>Ingredients</label>
-        {recipe.ingredients.map((ingredient, index)=>(
-            <input key={index} name='ingredients' value={ingredient} onChange={(e)=>handleIngredientChange(e, index)}/>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" onChange={handleChange} />
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="ingredients">Ingredients</label>
+        {recipe.ingredients.map((ingredient, index) => (
+          <input
+            key={index}
+            name="ingredients"
+            value={ingredient}
+            onChange={(e) => handleIngredientChange(e, index)}
+          />
         ))}
-        <button onClick={addIngredient} type='button'>Add Ingredient</button>
-        <label htmlFor='instructions'>Instructions</label>
-        <textarea id='instructions' name='instructions' onChange={handleChange}></textarea>
-        <label htmlFor='imageUrl'>Image URL</label>
-        <input id='imageUrl' name='imageUrl' onChange={handleChange} />
-        <label htmlFor='cookingTime'>Cooking Time (minutes)</label>
-        <input type='number' id='cookingTime' name='cookingTime' onChange={handleChange} />
-        <button type='submit'>Create</button>
+        <button onClick={addIngredient} type="button">
+          Add Ingredient
+        </button>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input id="imageUrl" name="imageUrl" onChange={handleChange} />
+        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleChange}
+        />
+        <button type="submit">Create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreateRecipe
+export default CreateRecipe;
